6f388fc561d89b3925dbb142906ae77745d63744
gomoot_tha <gomoot_tha@truecorp.co.th>
2020-01-18 14:35:12 +0700

    fix weekdays

--- src/App.js
+++ src/App.js
@@ -1,99 +1,23 @@
-import React, { Component, useState, useCallback } from 'react';
+import React, { Component } from 'react';
 import DateTest from './DateTest';
-// pick a date util library
-//import DateFnsUtils from '@date-io/date-fns'; // choose your lib
-// import moment from 'moment';
-// import MomentUtils from '@date-io/moment';
-// import 'moment/locale/th';
+import DateTestEn from './DateTestEn';
 
-import {
-  DatePicker,
-  TimePicker,
-  DateTimePicker,
-  MuiPickersUtilsProvider
-} from '@material-ui/pickers';
-
-// class LocalizedUtils extends MomentUtils {
-//   getDatePickerHeaderText(date) {
-//     console.log(moment(date).format('DD MMM'));
-//     return moment(date).format('DD MMM');
-//     //moment(date).fotmat("d MMM yyyy", { locale: this.locale });
-//   }
-//   getDateTimePickerHeaderText(date) {
-//     console.log(moment(date).format('DD MMM'));
-//     return moment(date).format('DD MMM');
-//     //moment(date).fotmat("d MMM yyyy", { locale: this.locale });
-//   }
-// }
 
 class App extends Component {
   constructor(props) {
     super(props);
-    //  const [locale, setLocale] = useState("fr");
-//    let dd = moment().add('years', 543);
-
-    // this.state = {
-    //   selectedDate: moment()
-    //     .add('years', 543)
-    //     .toDate(),
-    //   maxDate: moment()
-    //     .add('years', 545)
-    //     .toDate(),
-    //   selectedDateEn: moment().toDate(),
-    //   maxDateEn: moment()
-    //     .add('years', 2)
-    //     .toDate(),
-    //   locale: props
-    //   //localeEn: 'en',
-    // };
-// 
-    // console.log(this.state.selectedDate);
+   
   }
 
-  // //const [selectedDate, handleDateChange] = useState(new Date());
-  // handleDateChange = newDate => {
-  //   console.log(moment(newDate).format('DD MMM'));
-  //   this.setState({ selectedDate: newDate });
-  // };
-
-  // handleDateChangeEn = newDate => {
-  //   this.setState({ selectedDateEn: newDate });
-  // };
 
   render() {
     //MomentUtils
     // MomentUtils.locale = this.state.locale;
     return (
-      
-      // <MuiPickersUtilsProvider
-      //   libInstance={moment}
-      //   utils={LocalizedUtils}
-      //   // locale={this.state.locale}
-      // >
-      //   <DateTimePicker
-      //     value={this.state.selectedDate}
-      //     onChange={this.handleDateChange}
-      //     ampm={false}
-      //     locale={this.state.locale}
-      //     variant={'inline'} // dialog
-      //     minDate={this.state.selectedDate}
-      //     maxDate={this.state.maxDate}
-      //     format='DD MMM YYYY HH:mm'
-      //   />
-      //   <DateTimePicker
-      //     value={this.state.selectedDateEn}
-      //     onChange={this.handleDateChangeEn}
-      //     ampm={false}
-      //     locale={this.state.localeEn}
-      //     variant={'inline'} // dialog
-      //     minDate={this.state.selectedDateEn}
-      //     maxDate={this.state.maxDateEn}
-      //     format='DD MMM YYYY HH:mm'
-      //   />
-      // </MuiPickersUtilsProvider>
       <div>
         <DateTest locale='th' />
         <DateTest locale='en' />
+        {/* <DateTestEn locale='en' /> */}
       </div>
     );
   }
--- src/DateTestEn.js
+++ src/DateTestEn.js
@@ -1,68 +1,50 @@
-/*
- * Gomoot T.
- */
 import React, { Component } from 'react';
-import MomentUtils from '@date-io/moment';
+
 import moment from 'moment';
-import 'moment/locale/th';
-import { DateTimePicker, MuiPickersUtilsProvider } from '@material-ui/pickers';
+import MomentUtils from '@date-io/moment';
 
-export class ThUtils extends MomentUtils {
+import {DateTimePicker,MuiPickersUtilsProvider} from '@material-ui/pickers';
+
+export  class LocalizedUtilsEn extends MomentUtils {
   constructor(props) {
-    super(props);
-    this.locale = 'th';
-    this.moment.locale('th');
-  }
-
-  getWeekdays() {
-    this.moment.locale('th');
-    return this.moment.weekdaysShort(true);
-  }
-  getDatePickerHeaderText(date) {
-    // console.log(moment(date).format('DD MMM'));
-    return moment(date).format('DD MMM');
-  }
-  
-  getDateTimePickerHeaderText(date) {
-    return moment(date).format('DD MMM');
-  }
-}
-
-export class EnUtils extends MomentUtils {
-  constructor(props) {
-    super(props);
+    super(props); 
+   // this.moment.locale = 'en';
     this.locale = 'en';
+    console.log( 'LocalizedUtilsEn ' + this.getWeekdays());
     this.moment.locale('en');
+    console.log(this.moment.weekdaysShort(false));
   }
 
   getWeekdays() {
     this.moment.locale('en');
+    console.log(this.locale);
+    console.log(super.getWeekdays());
+    console.log( this.moment.weekdaysShort(true) );
+    
     return this.moment.weekdaysShort(false);
   }
   getDatePickerHeaderText(date) {
     console.log(moment(date).format('DD MMM'));
     return moment(date).format('DD MMM');
+    //moment(date).fotmat("d MMM yyyy", { locale: this.locale });
   }
   getDateTimePickerHeaderText(date) {
     console.log(moment(date).format('DD MMM'));
     return moment(date).format('DD MMM');
+    //moment(date).fotmat("d MMM yyyy", { locale: this.locale });
   }
 }
 
-class DateTest extends Component {
+class DateTestEn extends Component {
   constructor(props) {
     super(props);
     let addYear = props.locale == 'th' ? 543 : 0;
     this.state = {
-      selectedDate: moment()
-        .add('years', addYear)
-        .toDate(),
-      maxDate: moment()
-        .add('years', addYear + 2)
-        .toDate(),
-      locale: props.locale
+      selectedDate: moment().add('years', addYear).toDate(),
+      maxDate: moment().add('years', addYear+2).toDate(),
+      locale: props.locale,
     };
-    moment.locale(props.locale);
+   // moment.locale(props.locale);
     console.log('constru');
   }
 
@@ -73,19 +55,19 @@ class DateTest extends Component {
   };
 
   render() {
-    //console.log('render' + this.props.locale);
-    moment.locale(this.props.locale);
+    console.log('render' + this.props.locale);
+   // moment.locale(this.props.locale);
     return (
       <MuiPickersUtilsProvider
         libInstance={moment}
-        utils={this.props.locale === 'th' ? ThUtils : EnUtils}
+        utils={LocalizedUtilsEn}
         locale={this.props.locale}
       >
         <DateTimePicker
           value={this.state.selectedDate}
           onChange={this.handleDateChange}
           ampm={false}
-          // locale={this.props.locale}
+          locale={this.props.locale}
           variant={'inline'} // dialog
           minDate={this.state.selectedDate}
           maxDate={this.state.maxDate}
@@ -96,4 +78,4 @@ class DateTest extends Component {
   }
 }
 
-export default DateTest;
+export default DateTestEn;
